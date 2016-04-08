e7085ad795295144a392895c36140b68bc9af2ba
furplag <furplag@users.noreply.github.com>
2016-04-08 14:49:25 +0900

    Update jdk.6.install.sh

--- rhel/jdk.6.install.sh
+++ rhel/jdk.6.install.sh
@@ -5,9 +5,9 @@
 # Copyright 2016 furplag
 # Licensed under MIT (https://github.com/furplag/linux-config-tips/blob/master/LICENSE)
 
-# Automatically setting up to Java.
+# Automatically setting up to use Java.
 # 1. Downloading JDK only use command-line.
-# 2. Does not remove previous version of JDK, if yum update jdk runs.
+# 2. Does not remove previous version of JDK, if "yum update jdk" runs.
 # 3. Set "alternatives" for JDK.
 # 4. Set $JAVA_HOME (relate to alternatives config).
 
@@ -20,15 +20,17 @@ installJDK=1.$installVer.0_$installUVer
 installRoot=/usr/java
 priority=`echo $installJDK | sed -e "s/[\.]//g" | sed -e "s/_/0/"`
 
-echo -e "Oracle JDK ${installVer}u${installUVer} install with alternatives\n"
+if [ ! ${EUID:-${UID}} = 0 ]; then echo -e "\nPermission Denied, Root user only.\nHint: sudo"; exit 0; fi
+
+echo -e "Oracle JDK ${installVer}u${installUVer} install with alternatives.\n"
 
 echo -e "\n  # Checking installed package named \"jdk\".\n"
-if [ "${conflictPackage=$(rpm -qa jdk)}" ]; then
+if [ "${conflictPackage=$(rpm -qa jdk | grep x64)}" ]; then
   conflictJDK=`echo $conflictPackage | cut -d "-" -f 2`
   conflictVer=`echo $conflictJDK | cut -d "." -f 2`
   conflictUVer=`echo $conflictVer | cut -d "_" -f 2`
   if [ $installVer -gt $conflictVer ]; then
-    echo "    Ah, we have encountered Prehistoric JDK now !"
+    echo "    Ah, we have encountered Prehistoric JDK now !\n"
   fi
   echo -e "    ${conflictPackage} already installed."
 fi
@@ -58,10 +60,8 @@ elif [ -e /usr/java/jdk$installJDK ]; then
   resourceURL=
 fi
 
-
-
 if [ ! -e /tmp/$resource ] && [ "${resourceURL}" ]; then
-  echo -e "\n  # Download Oracle JDK ${installVer} without single-sign-on\n" && \
+  echo -e "\n  # Download Oracle JDK ${installVer}.\n" && \
     wget $resourceURL \
       --no-check-certificate \
       --no-cookies \
@@ -69,9 +69,19 @@ if [ ! -e /tmp/$resource ] && [ "${resourceURL}" ]; then
       -qO /tmp/$resource
   if [ -e /tmp/$resource ] && [ `echo $resource | grep -e "bin$"` ]; then
     echo "    unpacking ${resource} ..." && \
-    chmod +x /tmp/$resource && \
-    installSource=`unzip -l /tmp/$resource -x sun-javadb* 2>/dev/null | grep rpm | grep -v -e "${resource}$" | sed -e "s/.*\s//"`
-    unzip -o /tmp/$resource -d /tmp -x sun-javadb* >/dev/null 2>&1
+    chmod +x /tmp/$resource
+    if [ `echo $resource | grep -rpm` ]; then
+      installSource=`unzip -l /tmp/$resource -x sun-javadb* 2>/dev/null | grep rpm | grep -v -e "${resource}$" | sed -e "s/.*\s//"`
+      unzip -o /tmp/$resource -d /tmp -x sun-javadb* >/dev/null 2>&1
+    else
+      /tmp/$resource -x
+      installSource="jdk${installJDK}"
+      if [ -e "${installSource}" ]; then
+        mv $installSource /tmp
+      else
+        installSource=
+      fi
+    fi
   elif [ -e /tmp/$resource ]; then
     installSource=$resource
   fi
@@ -87,52 +97,16 @@ if [ "${installSource}" ] && [ -e /tmp/$installSource ]; then
   fi
 fi
 
-echo "  # Setting up \"alternatives\" for \"java\"."
-
-alternatives --remove java /usr/java/jdk$installJDK/bin/java >/dev/null 2>&1
-alternatives --install /usr/bin/java java /usr/java/jdk$installJDK/bin/java $priority \
- --slave /usr/bin/appletviewer appletviewer /usr/java/jdk$installJDK/bin/appletviewer \
- --slave /usr/bin/extcheck extcheck /usr/java/jdk$installJDK/bin/extcheck \
- --slave /usr/bin/idlj idlj /usr/java/jdk$installJDK/bin/idlj \
- --slave /usr/bin/jar jar /usr/java/jdk$installJDK/bin/jar \
- --slave /usr/bin/jarsigner jarsigner /usr/java/jdk$installJDK/bin/jarsigner \
- --slave /usr/bin/javac javac /usr/java/jdk$installJDK/bin/javac \
- --slave /usr/bin/javadoc javadoc /usr/java/jdk$installJDK/bin/javadoc \
- --slave /usr/bin/javah javah /usr/java/jdk$installJDK/bin/javah \
- --slave /usr/bin/javap javap /usr/java/jdk$installJDK/bin/javap \
- --slave /usr/bin/jconsole jconsole /usr/java/jdk$installJDK/bin/jconsole \
- --slave /usr/bin/jdb jdb /usr/java/jdk$installJDK/bin/jdb \
- --slave /usr/bin/jhat jhat /usr/java/jdk$installJDK/bin/jhat \
- --slave /usr/bin/jinfo jinfo /usr/java/jdk$installJDK/bin/jinfo \
- --slave /usr/bin/jmap jmap /usr/java/jdk$installJDK/bin/jmap \
- --slave /usr/bin/jps jps /usr/java/jdk$installJDK/bin/jps \
- --slave /usr/bin/jrunscript jrunscript /usr/java/jdk$installJDK/bin/jrunscript \
- --slave /usr/bin/jsadebugd jsadebugd /usr/java/jdk$installJDK/bin/jsadebugd \
- --slave /usr/bin/jstack jstack /usr/java/jdk$installJDK/bin/jstack \
- --slave /usr/bin/jstat jstat /usr/java/jdk$installJDK/bin/jstat \
- --slave /usr/bin/jstatd jstatd /usr/java/jdk$installJDK/bin/jstatd \
- --slave /usr/bin/jvisualvm jvisualvm /usr/java/jdk$installJDK/bin/jvisualvm \
- --slave /usr/bin/native2ascii native2ascii /usr/java/jdk$installJDK/bin/native2ascii \
- --slave /usr/bin/rmic rmic /usr/java/jdk$installJDK/bin/rmic \
- --slave /usr/bin/schemagen schemagen /usr/java/jdk$installJDK/bin/schemagen \
- --slave /usr/bin/serialver serialver /usr/java/jdk$installJDK/bin/serialver \
- --slave /usr/bin/wsgen wsgen /usr/java/jdk$installJDK/bin/wsgen \
- --slave /usr/bin/wsimport wsimport /usr/java/jdk$installJDK/bin/wsimport \
- --slave /usr/bin/xjc xjc /usr/java/jdk$installJDK/bin/xjc \
- --slave /usr/bin/ControlPanel ControlPanel /usr/java/jdk$installJDK/jre/bin/ControlPanel \
- --slave /usr/bin/javaws javaws /usr/java/jdk$installJDK/jre/bin/javaws \
- --slave /usr/bin/jcontrol jcontrol /usr/java/jdk$installJDK/jre/bin/jcontrol \
- --slave /usr/bin/keytool keytool /usr/java/jdk$installJDK/jre/bin/keytool \
- --slave /usr/bin/orbd orbd /usr/java/jdk$installJDK/jre/bin/orbd \
- --slave /usr/bin/pack200 pack200 /usr/java/jdk$installJDK/jre/bin/pack200 \
- --slave /usr/bin/policytool policytool /usr/java/jdk$installJDK/jre/bin/policytool \
- --slave /usr/bin/rmid rmid /usr/java/jdk$installJDK/jre/bin/rmid \
- --slave /usr/bin/rmiregistry rmiregistry /usr/java/jdk$installJDK/jre/bin/rmiregistry \
- --slave /usr/bin/servertool servertool /usr/java/jdk$installJDK/jre/bin/servertool \
- --slave /usr/bin/tnameserv tnameserv /usr/java/jdk$installJDK/jre/bin/tnameserv \
- --slave /usr/bin/unpack200 unpack200 /usr/java/jdk$installJDK/jre/bin/unpack200
+if [ -e /usr/java/jdk$installJDK ] && [ `/usr/java/jdk$installJDK/bin/java -version 2>/dev/null | wc -l` -gt 0 ]; then
+  echo "  # Setting up \"alternatives\" for \"java\"."
+  wget https://raw.githubusercontent.com/furplag/linux-config-tips/rhel/jdk.6.alternatives.sh && \
+    chmod +x jdk.6.alternatives.sh && \
+    .jdk.6.alternatives.sh $installJDK
+  rm -f jdk.6.alternatives.sh
+fi
 
 [ ! -e /etc/profile.d/java.sh ] && \
+echo -e "    Set Environment \$JAVA_HOME (relate to alternatives config)."
 cat <<_EOT_ > /etc/profile.d/java.sh
 # Set Environment with alternatives for Java VM.
 export JAVA_HOME=\$(readlink /etc/alternatives/java | sed -e 's/\/bin\/java//g')
