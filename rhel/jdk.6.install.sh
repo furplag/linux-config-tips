179cf9075c59e1c8f39b6f5ad500589b72c65c36
furplag <furplag@users.noreply.github.com>
2016-04-08 17:23:33 +0900

    Update jdk.6.install.sh

--- rhel/jdk.6.install.sh
+++ rhel/jdk.6.install.sh
@@ -25,7 +25,7 @@ if [ ! ${EUID:-${UID}} = 0 ]; then echo -e "Permission Denied, Root user only.\n
 echo -e "Oracle JDK ${installVer}u${installUVer} install with \"alternatives java\".\n"
 
 echo -e "\n  # Checking installed package named \"jdk\".\n"
-if [ "${conflictPackage=$(rpm -qa jdk | grep x64)}" ]; then
+if [ "${conflictPackage=$(rpm -qa jdk | grep x86_64)}" ]; then
   conflictJDK=`echo $conflictPackage | cut -d "-" -f 2`
   conflictVer=`echo $conflictJDK | cut -d "." -f 2`
   conflictUVer=`echo $conflictVer | cut -d "_" -f 2`
