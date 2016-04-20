c479e8f8aaaf991b6252e6438837042337955f91
furplag <furplag@users.noreply.github.com>
2016-04-20 14:43:05 +0900

    Update mvn.install.sh

--- rhel/java/mvn.install.sh
+++ rhel/java/mvn.install.sh
@@ -6,7 +6,9 @@
 # Licensed under MIT (https://github.com/furplag/linux-config-tips/blob/master/LICENSE)
 
 # Install Apache Maven relate \$JAVA_HOME
-# 
+# 1. Download Apache Maven 3.
+# 3. Set "alternatives" for Maven.
+# 4. Set $M2, $M2_HOME (relate to $JAVA_HOME).
 
 # variables
 javaVer=((java -version 2>&1) | grep -i version | cut -d '"' -f 2)
@@ -16,6 +18,8 @@ elif [ $JAVA_HOME ]; then
   javaVer=(($JAVA_HOME/bin/java -version 2>&1) | grep -i version | cut -d '"' -f 2)
   jdkVer=$javaVer | cut -d '.' -f 2
 fi
+
+if [ ! ${EUID:-${UID}} = 0 ]; then echo -e "Permission Denied, Root user only.\nHint: sudo ${0}"; exit 0; fi
 
 if [ ! $jdkVer ]; then
   echo -e "\n  Java VM not difined, Install JDK first."
@@ -44,14 +48,32 @@ for mavenVer in 3.2.5 3.3.9; do
    --slave /usr/local/bin/mvnyjp mvnyjp /usr/local/maven/apache-maven-$mavenVer/bin/mvnyjp
   if [ $(((alternatives --display mvn) | grep $mavenVer/bin/mvn | wc -l)) -gt 0 ]; then
     echo "    maven-${mavenVer} installed in \"/usr/local/maven\"."
-    [ -e /etc/profile.d/mvn.sh ] || cat << _EOT_ > /etc/profile.d/mvn.sh
+  else
+    echo "    maven-${mavenVer} install failed."
+  fi
+done
+
+[ -e /etc/profile.d/mvn.sh ] || cat << _EOT_ > /etc/profile.d/mvn.sh
 #/etc/profile.d/mvn.sh
 
 # Set Environment with alternatives for Apache Maven.
-[ $JAVA_HOME ] || exit 0
-export JAVA_HOME=\$(readlink /etc/alternatives/java | sed -e 's/\/bin\/java//g')
+[ $(((which mvn 2>&1) | grep -e "\/bin\/java" | wc -l)) -gt 0 ] || exit 0
+if [ \$JAVA_HOME ]; then
+  jdkVer=((\$JAVA_HOME/bin/java -version 2>&1) | grep -i version | cut -d '"' -f 2 | cut -d '.' -f 2)
+elif [ $(((which java 2>&1) | grep -e "\/bin\/java" | wc -l)) -gt 0 ]; then
+  export JAVA_HOME=((which java 2>&1) | sed -e 's/\/bin\/java//')
+  jdkVer=((\$JAVA_HOME/bin/java -version 2>&1) | grep -i version | cut -d '"' -f 2 | cut -d '.' -f 2)
+fi
+
+if [ \$((jdkVer)) -gt 6 ]; then
+  alternatives --set mvn /usr/local/maven/apache-maven-3.3.9/bin/mvn
+elif [ \$((jdkVer)) -gt 4 ]; then
+  alternatives --set mvn /usr/local/maven/apache-maven-3.2.5/bin/mvn
+else
+  exit 0
+fi
+export M2_HOME=(readlink -m \$(which mvn) | sed -e 's/\/bin\/mvn//')
+export M2=\$M2_HOME/bin
+
 _EOT_
-
-done
-
 
