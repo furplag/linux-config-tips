0edd6b383ce5b7688ff15226844444a87e96e88a
furplag <furplag@users.noreply.github.com>
2016-05-09 14:51:02 +0900

    Create alternatives.install.sh

---
+++ rhel/java/alternatives.install.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+set -ue -o pipefail
+export LC_ALL=C
+
+# variables
+declare -r name=`basename $0`
+declare -r javaHomeDefault=/usr/java/default
+declare javaHome=
+declare quietly=false
+declare version=
+declare -i ver=
+
+# Usage
+usage(){
+  cat << _EOT_
+${name}
+
+Description:
+  1. Set "alternatives" for JDK.
+Requirement:
+  1. root user executable only.
+  2. Installable JDK version: 5 - 8.
+usgae: ${name} [-d javaHome] [-q]
+  -d : javaHome (optional, default: $javaHomeDefault)
+       absolute path of java ( e.g. /usr/java/jdk1.8.0_92) .
+  -q : running quietly.
+_EOT_
+}
+
+while getopts dq: OPT; do
+  case $OPT in
+    d) javaHome=${OPTARG:-${javaHomeDefault}};;
+    q) quietly=true;;
+    h) usage; exit 1;;
+    \?) usage; exit 1;;
+  esac
+done
+
+if [ -z "${javaHome}" ]; then
+  [ $quietly ] || usage
+  exit 1
+elif [ ! -e $javaHome ]; then
+  [ $quietly ] || echo -e "\n  \"${javaHome}\" is not Java home."
+  exit 1
+fi
+
+version=$($jdkHome/bin/java -version 2>&1 | grep -i version | cut -d '"' -f 2)
+if [ -z "${version}" ]; then
+  [ $quietly ] || echo -e "\n  \"${javaHome}\" is not Java home."
+  exit 1
+fi
+
+ver=$(echo $version | cut -d '.' -f 2)
+
+if [ $ver -eq 0 ]; then
+  [ $quietly ] || echo -e "\n  \"${javaHome}\" is not Java home."
+  exit 1
+fi
+
+exit 0
